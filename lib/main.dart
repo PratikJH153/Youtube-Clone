13ca8131104ce5a2dd8144dd1944a252593a64a6
PratikJH <pratik.jh2017@gmail.com>
2021-12-18 15:05:57 +0530

    Added all files

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../views/home_page.dart';
+import '../views/video_view_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Youtube App",
+      initialRoute: '/',
+      routes: {
+        HomePage.routeName: (ctx) => const HomePage(),
+        VideoViewPage.routeName: (ctx) => const VideoViewPage(),
+      },
+    );
+  }
+}
---
+++ lib/views/video_view_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class VideoViewPage extends StatelessWidget {
+  static const routeName = '/video-view';
+  const VideoViewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
